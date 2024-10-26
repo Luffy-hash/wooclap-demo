d5b2c517d8f717303ad2e4a0814f9ac82134f1ec
Luffy-hash <ba654abdoulaye@gmail.com>
2024-10-26 09:29:58 +0200

    creation et initialisation : debut de projet avec le front et le back sous angular et spring

---
+++ wooclap-back/src/main/java/fr/orleans/m1/wooclap/wooclapback/WooclapBackApplication.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wooclap.wooclapback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooclapBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooclapBackApplication.class, args);
+    }
+
+}
---
+++ wooclap-back/src/test/java/fr/orleans/m1/wooclap/wooclapback/WooclapBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wooclap.wooclapback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooclapBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5706874968e9707981fe805315fb232afa6b5fac
Luffy-hash <ba654abdoulaye@gmail.com>
2024-10-26 09:13:59 +0200

    creation et initialisation du projet wooclap côté back et front.

---
+++ wooclap-backend/src/test/java/fr/orleans/m1/wooclap/wooclapbackend/WooclapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orleans.m1.wooclap.wooclapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooclapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
